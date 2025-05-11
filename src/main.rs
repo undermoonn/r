b9138082717d72e236c7e858c52a79401b8fcafb
undermoonn <under-moon@outlook.com>
2025-05-11 22:01:54 +0800

    guess number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::cmp::Ordering;
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number! ");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please enter your guess: ");
+
+        let mut guess = String::new();
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You guessed it!");
+                break;
+            }
+        }
+    }
 }
